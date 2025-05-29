f60b6a4675397783f6a90917021ef5c8537d980b
doomling <bethlejem@gmail.com>
2025-05-29 00:49:11 +0000

    feat/add greeter

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Greeter from "./components/Greeter";
 
-function App() {
+  function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
+        <Greeter />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
